9c8638d81fb2957148f8d9234983da3494ad5f0f
Peter <mcheung63@hotmail.com>
2015-10-25 02:12:55 +0800

    hard code tree cell color

--- src/com/peter/mavenrunner/MyTreeNodeRenderer.java
+++ src/com/peter/mavenrunner/MyTreeNodeRenderer.java
@@ -5,10 +5,10 @@
  */
 package com.peter.mavenrunner;
 
+import java.awt.Color;
 import java.awt.Component;
 import javax.swing.JLabel;
 import javax.swing.JTree;
-import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 import javax.swing.tree.TreeCellRenderer;
 import org.netbeans.api.project.ProjectInformation;
@@ -20,8 +20,8 @@ import org.netbeans.api.project.ProjectInformation;
 public class MyTreeNodeRenderer extends JLabel implements TreeCellRenderer {
 
 	public MyTreeNodeRenderer() {
-		setBorder(new EmptyBorder(5, 5, 5, 5));
-		//setOpaque(true);
+		setBorder(new EmptyBorder(4, 4, 4, 4));
+		setOpaque(true);
 	}
 
 	@Override
@@ -34,12 +34,19 @@ public class MyTreeNodeRenderer extends JLabel implements TreeCellRenderer {
 			setText(treeNode.name);
 		}
 		setIcon(treeNode.icon);
-		if (selected) {
+//		if (selected) {
 //			setBackground(UIManager.getColor("Tree.selectionBackground"));
-			setForeground(UIManager.getColor("Tree.selectionForeground"));
-		} else {
+//			setForeground(UIManager.getColor("Tree.selectionForeground"));
+//		} else {
 //			setBackground(UIManager.getColor("Tree.background"));
-			setForeground(UIManager.getColor("Tree.foreground"));
+//			setForeground(UIManager.getColor("Tree.foreground"));
+//		}
+		if (selected) {
+			setBackground(new Color(9, 80, 208));
+			setForeground(Color.white);
+		} else {
+			setBackground(Color.white);
+			setForeground(Color.black);
 		}
 		return this;
 	}
